6c4667657c0e783e737f5f2fe0d50b9ea9742b90
Mihi <devdumbblind@googlemail.com>
2016-07-01 16:15:37 +0800

    removed unused SelectionView

--- Viticulture/Services/PlayerSelection.cs
+++ Viticulture/Services/PlayerSelection.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.Composition;
-using System.Linq;
-using System.Text;
+﻿using System.ComponentModel.Composition;
 using System.Threading.Tasks;
 using MahApps.Metro.Controls.Dialogs;
-using Viticulture.Components.Game;
 
 namespace Viticulture.Services
 {
@@ -13,25 +8,16 @@ namespace Viticulture.Services
     public class PlayerSelection : IPlayerSelection
     {
         private readonly IMetroDialog _metroDialog;
-        private readonly IMefContainer _mefContainer;
+
 
         [ImportingConstructor]
-        public PlayerSelection(IMetroDialog metroDialog, IMefContainer mefContainer)
+        public PlayerSelection(IMetroDialog metroDialog)
         {
             _metroDialog = metroDialog;
-            _mefContainer = mefContainer;
         }
 
         public async Task<Selection> Select(string title, string message, string option1, string option2)
         {
-
-            var selectionViewModel = _mefContainer.GetExportedValue<ISelectionViewModel>();
-
-            selectionViewModel.Title = title;
-            selectionViewModel.Message = message;
-            selectionViewModel.Option1 = option1;
-            selectionViewModel.Option2 = option2;
-
             var result = await
                 _metroDialog.ShowMessage(title, message, MessageDialogStyle.AffirmativeAndNegative,
                     new MetroDialogSettings { AffirmativeButtonText = option1, NegativeButtonText = option2 });
